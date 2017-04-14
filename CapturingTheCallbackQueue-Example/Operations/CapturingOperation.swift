ebcacd73c4e3bf29979a9bb23b7d0795f969a1cd
William Boles <william@memrise.com>
2017-04-14 15:20:23 +0100

    Implemented a queue/thread capturing operation

---
+++ CapturingTheCallbackQueue-Example/Operations/CapturingOperation.swift
@@ -0,0 +1,40 @@
+//
+//  CapturingOperation.swift
+//  CapturingTheCallbackQueue-Example
+//
+//  Created by William Boles on 14/04/2017.
+//  Copyright © 2017 William Boles. All rights reserved.
+//
+
+import Foundation
+
+class CapturingOperation: Operation {
+    
+    var completion : ((_ successful: Bool) -> (Void))?
+    var callBackQueue: OperationQueue
+    
+    //MARK: - Init
+    
+    init(completion: ((_ successful: Bool) -> Void)?) {
+        self.completion = completion
+        self.callBackQueue = OperationQueue.current!
+        
+        super.init()
+    }
+    
+    // MARK: - Main
+    
+    override func main() {
+        super.main()
+        
+        sleep(2) //This is where this operation's actual work would be
+        
+        callBackQueue.addOperation {
+            guard let completion = self.completion else {
+                return
+            }
+            
+            completion(true)
+        }
+    }
+}
--- CapturingTheCallbackQueue-Example/ViewControllers/ViewController.swift
+++ CapturingTheCallbackQueue-Example/ViewControllers/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var updateLabel: UILabel!
+    
+    let queue: OperationQueue = OperationQueue()
+    
+    // MARK: - ViewLifecycle
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let operationA = CapturingOperation { (successful: Bool) in
+            self.updateLabel.text = "This label has been updated from the following queue: \(OperationQueue.current!)"
+        }
+        
+        queue.addOperation(operationA)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
